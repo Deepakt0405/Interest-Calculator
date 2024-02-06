c4fd1bda985a11d4039f87fd299e66e93c746be3
Deepak Tiwari <tiwarideepak0405@gmail.com>
2024-02-06 21:57:47 +0530

    Error Fixed

--- lib/main.dart
+++ lib/main.dart
@@ -5,31 +5,30 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Interest Calculator',
       theme: ThemeData(
-        primarySwatch: Colors.teal,
-        hintColor: Colors.tealAccent,
+        primarySwatch: Colors.blue,
+        hintColor: Colors.blueAccent,
       ),
-      home: Form(),
+      home: const FormWidget(),
     );
   }
 }
 
-class Form extends StatefulWidget {
-  const Form({super.key});
+class FormWidget extends StatefulWidget {
+  const FormWidget({Key? key});
 
   @override
-  State<Form> createState() => _MyWidgetState();
+  State<FormWidget> createState() => _FormWidgetState();
 }
 
-class _MyWidgetState extends State<Form> {
+class _FormWidgetState extends State<FormWidget> {
   TextEditingController principalTextEditingController =
       TextEditingController();
   TextEditingController rateOfInterestTextEditingController =
@@ -45,14 +44,20 @@ class _MyWidgetState extends State<Form> {
   String nv = "";
 
   @override
+  void initState() {
+    currentValue = _currencies[0];
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Interest Calculator"),
+        title: const Text("Interest Calculator"),
         centerTitle: true,
       ),
       body: Container(
-        margin: EdgeInsets.all(10),
+        margin: const EdgeInsets.all(10),
         child: ListView(
           children: <Widget>[
             //Image
@@ -60,94 +65,151 @@ class _MyWidgetState extends State<Form> {
 
             Row(
               children: <Widget>[
-                Expanded(child: Padding(
-                  padding: EdgeInsets.all(1.0),
-                  child: ListTile(
-                    title: Text("Simple Interest"),
-                    leading: Radio(
-                      value: "Simple",
-                      groupValue: _character,
-                      onChanged: (value)
-                      {
-                        setState(() {
-                          // Here it is simple
-                          _character = value!;
-                        });
-
-                      },
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(1.0),
+                    child: ListTile(
+                      title: const Text("Simple Interest"),
+                      leading: Radio(
+                        value: "Simple",
+                        groupValue: _character,
+                        onChanged: (value) {
+                          setState(() {
+                            // Here it is simple
+                            _character = value!;
+                          });
+                        },
+                      ),
                     ),
                   ),
-                )),
-                Expanded(child: Padding(
-                  padding: EdgeInsets.all(1.0),
-                  child: ListTile(
-                    title: Text("Compound Interest"),
-                    leading: Radio(
-                      value: "Compound",
-                      groupValue: _character,
-                      onChanged: (value)
-                      {
-                        setState(() {
-                          // Here it is simple
-                          _character = value!;
-                        });
-
-                      },
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(1.0),
+                    child: ListTile(
+                      title: const Text("Compound Interest"),
+                      leading: Radio(
+                        value: "Compound",
+                        groupValue: _character,
+                        onChanged: (value) {
+                          setState(() {
+                            // Here it is simple
+                            _character = value!;
+                          });
+                        },
+                      ),
                     ),
                   ),
-                )),
+                ),
                 Container(
                   width: 5.0,
                 ),
               ],
             ),
-            Padding(padding: EdgeInsets.all(5),
+
+            Padding(
+              padding: const EdgeInsets.all(5),
               child: TextField(
-                style: TextStyle(color: Colors.black),
+                style: const TextStyle(color: Colors.black),
                 keyboardType: TextInputType.number,
                 controller: principalTextEditingController,
                 decoration: InputDecoration(
+                  labelStyle: const TextStyle(color: Colors.black),
                   labelText: "Principal",
+                  hintStyle: const TextStyle(color: Colors.grey),
                   hintText: "Enter a principal amount e.g. 1099",
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(5.0),
-                  )
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
                 ),
               ),
             ),
-            Padding(padding: EdgeInsets.all(5),
+
+            Padding(
+              padding: const EdgeInsets.all(5),
               child: TextField(
-                style: TextStyle(color: Colors.black),
+                style: const TextStyle(color: Colors.black),
                 keyboardType: TextInputType.number,
-                controller: principalTextEditingController,
+                controller: rateOfInterestTextEditingController,
                 decoration: InputDecoration(
-                  style: TextStyle(color: Colors.black),
-                    labelText: "Rate Of Interest",
-                    hintText: "Enter a rate per year",
-                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(5.0),
-                    )
+                  labelStyle: const TextStyle(color: Colors.black),
+                  labelText: "Rate Of Interest",
+                  hintStyle: const TextStyle(color: Colors.grey),
+                  hintText: "Enter a rate per year",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
                 ),
               ),
             ),
-            Row(children: <Widget>[
-              Expanded(child: Padding(padding:EdgeInsets.all(5),
-              ))
-            ],)
+
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(5),
+                    child: TextField(
+                      style: const TextStyle(color: Colors.black),
+                      keyboardType: TextInputType.number,
+                      controller: termTextEditingController,
+                      decoration: InputDecoration(
+                        labelStyle: const TextStyle(color: Colors.black),
+                        labelText: "Term",
+                        hintStyle: const TextStyle(color: Colors.grey),
+                        hintText: "Enter number of years",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 10,
+                ),
+                //dropdown menu
+                Expanded(
+                  child: DropdownButton<String>(
+                    items: _currencies.map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    value: currentValue,
+                    onChanged: (newValue) {
+                      _setSelectedValue(newValue!);
+                      this.nv = newValue;
+                      setState(() {
+                        this.currentValue = newValue;
+                      });
+                    },
+                  ),
+                )
+              ],
+            )
           ],
         ),
       ),
     );
   }
+
+  void _setSelectedValue(String newValue) {
+    setState(() {
+      this.currentValue = newValue;
+    });
+  }
 }
 
-Widget getImage(){
-    AssetImage assetImage = AssetImage('assets/back.png');
-    Image image = Image(
-      image: assetImage,
-      width: 150,
-      height: 150,
-    );
-    return Container(
-      child: image,
-      margin: EdgeInsets.all(40),
-    );
+Widget getImage() {
+  const AssetImage assetImage = AssetImage('assets/back.png');
+  final Image image = Image(
+    image: assetImage,
+    width: 150,
+    height: 150,
+  );
+  return Container(
+    child: image,
+    margin: const EdgeInsets.all(40),
+  );
 }
